1fb1322f38613065f3de0b27afc7c9d2162e41cc
Samuel Maina <mainasamnguru@gmail.com>
2025-04-03 02:26:27 +0300

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Employee_Management_System.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string EmployeeList { get; set; }
+        public string EmployeeSearch { get; set; }
+        public Employee()
+        {
+            
+        }
+
+    }
+}
